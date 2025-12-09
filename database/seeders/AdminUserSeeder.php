5c1b391298beea007626eabf4cb4704fac0608a7
JerryLee487 <carlosandresobandoguerra@gmail.com>
2025-12-09 17:19:34 -0500

    produccion

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        //1. Crear el rol Admin (si no existe)
+        $roleExists = DB::table('moonshine_user_roles')->where('name', 'Admin')->exists();
+
+        if (!$roleExists) {
+            $roleid = DB::table('moonshine_user_roles')->insertGetId([
+                'name' => 'Admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        } else {
+            $roleid = $roleExists->id; 
+        }
+
+        //2. Crear el usuario Admin(si no existe)
+        $userExists = DB::table('moonshine_users')->where('id', 1)->first();
+       
+        if (!$userExists) {
+            DB::table('moonshine_users')->insert([
+                'id' => 1,
+                'moonshine_user_role_id' => $roleid,
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password123'),
+                'avatar' => null,
+                'remember_token' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }    
+    }    
+}
