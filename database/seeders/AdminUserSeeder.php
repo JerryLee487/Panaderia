395a3fd5d9c33ca580304bf22c46a6a2915aa0c0
JerryLee487 <carlosandresobandoguerra@gmail.com>
2025-12-10 00:05:17 -0500

    subiendo a laravel cloud

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class AdminUserSeeder extends Seeder
